abbcfb8407c51221382e3c66e28bc203b230c71a
mcfilu <filip.1olszewski@gmail.com>
2023-08-21 18:13:10 +0100

    applying autoplaysinline to oconconeptinfo and pshotdetails plus chatbot to layout.js

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <script type="text/javascript" src="https://launcher.enquirybot.com/index.js" data-bot-id="a243d8ad-44a1-4441-aecd-b9b2a08ff886"></script>
+        </body>
     </html>
   )
 }
