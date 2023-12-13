b66ab594e8d8607116b999447c8ba6e4e721b149
mcfilu <filip.1olszewski@gmail.com>
2023-12-13 23:17:10 +0100

    the same update on the pshot video component

--- app/pshot/PshotVideo.jsx
+++ app/pshot/PshotVideo.jsx
@@ -16,7 +16,7 @@ const PshotVideo = () => {
           <div className={`flex flex-row justify-between w-full h-full mt-[2vh]`}>
          
               <div className={`flex flex-col justify-center items-center w-[55%] h-full md:p-[2vh]`}>
-                <p className={`text-gold1 text-[5vh] text-center font-header`}>P-Shot™ Injection Explained</p>
+                <p className={`text-gold1 text-[5vh] text-center font-header`}>P-Shot® UK Injection Explained</p>
                   <p className={`text-white text-left text-[2.5vh] mt-[7vh] md:pb-[5vh] font-main`}>We warmly invite you to watch a detailed video where Dr. Sherif Wakil, a highly respected expert in the field, carefully explains every part of the treatment process. In this video, Dr. Wakil breaks down the procedure into simple steps, using easy-to-understand language that makes the complex treatment procedure more accessible to everyone. This is an excellent opportunity to learn more about the procedure from an acknowledged authority in the field.</p>
                   <Link href="/book-now"><button className={` text-[3vh] px-[2vh] py-[0.5vh] bg-black border-gold1 border-2 text-gold1 hover:bg-white mt-[5vh] md:mt-[0px] font-main`}>Book Consultation</button></Link>
               </div>
@@ -36,7 +36,7 @@ const PshotVideo = () => {
           </div>
         </div>
         <div className={`flex flex-col md:hidden items-center`}>
-          <p className={`text-gold1 text-[4.5vh] text-center font-header`}>P-Shot™ Injection Explained</p>
+          <p className={`text-gold1 text-[4.5vh] text-center font-header`}>P-Shot® UK Injection Explained</p>
           {/* <iframe className={`w-[100%] h-[25vh] md:h-[100%]`} src="https://www.youtube.com/embed/WsHXew0UZUQ" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
           <LiteYouTubeEmbed
                   className={`w-full h-full`}
