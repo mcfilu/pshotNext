f21ae6dfc7070c59e57ac608d706f53c2b0f46a2
mcfilu <filip.1olszewski@gmail.com>
2023-12-13 23:16:36 +0100

    updating the p shot benefits and details pages on both mobile and dekstop, seo recommendations for html content

--- app/pshot/PshotBenefits.jsx
+++ app/pshot/PshotBenefits.jsx
@@ -11,8 +11,8 @@ const PshotBenefits = () => {
             <Image src={benefits2} objectFit="cover" objectPosition="center" fill="true" alt=""></Image>
         </div>
         <div className={`hidden md:flex flex flex-col w-[55%] p-[2vh]  items-center`}>
-            <h2 className={`text-gold1 text-[5vh] text-center font-header`}>P-Shot Benefits</h2>
-            <p className={`text-white font-poppins text-[2.1vh] text-justify mt-[3vh] mb-[2vh] font-main`}>The P-Shot offers a remarkable solution for individuals seeking long-term improvement in erectile dysfunction by effectively increasing blood flow to the penis. This innovative treatment can yield a multitude of benefits, such as:</p>
+            <h2 className={`text-gold1 text-[5vh] text-center font-header`}>P-Shot® Benefits</h2>
+            <p className={`text-white font-poppins text-[2.1vh] text-justify mt-[3vh] mb-[2vh] font-main`}>The London P-Shot® treatment offers a remarkable solution for individuals seeking long-term improvement in erectile dysfunction by effectively increasing blood flow to the penis. This innovative treatment can yield a multitude of benefits, such as:</p>
             <ul className={`text-white text-[2.1vh] font-poppins w-full items-left`}>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh] `}></i>Experience firmer erections for improved sexual performance and increased satisfaction.</li>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh]`}></i>Enjoy increased sensation, sex drive, and stamina for a more fulfilling sexual experience.</li>
@@ -26,12 +26,12 @@ const PshotBenefits = () => {
         
 
       <div className={`md:hidden flex flex-col items-center`}>
-      <h2 className={`text-gold1 text-[5vh] text-center font-header`}>P-Shot Benefits</h2>
+      <h2 className={`text-gold1 text-[5vh] text-center font-header`}>P-Shot® Benefits</h2>
       {/* <video src={boobs10} autoPlay loop muted className={` object-cover h-[100%] w-[100%]`}></video> */}
       <div className={`flex w-full h-[30vh] relative`}> 
         <Image src={benefits2} objectFit="cover" objectPosition="center" fill="true" alt=""></Image>
       </div>
-      <p className={`text-white font-poppins text-[2.1vh] text-left pl-[1vh] mt-[3vh] mb-[2vh] font-main`}>The P-Shot offers a remarkable solution for individuals seeking long-term improvement in erectile dysfunction by effectively increasing blood flow to the penis. This innovative treatment can yield a multitude of benefits, such as:</p>
+      <p className={`text-white font-poppins text-[2.1vh] text-left pl-[1vh] mt-[3vh] mb-[2vh] font-main`}>The London P-Shot® treatment offers a remarkable solution for individuals seeking long-term improvement in erectile dysfunction by effectively increasing blood flow to the penis. This innovative treatment can yield a multitude of benefits, such as:</p>
             <ul className={`text-white text-[2.1vh] font-poppins pl-[1vh]`}>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh] `}></i>Experience firmer erections for improved sexual performance and increased satisfaction.</li>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh] `}></i>Enjoy increased sensation, sex drive, and stamina for a more fulfilling sexual experience.</li>
